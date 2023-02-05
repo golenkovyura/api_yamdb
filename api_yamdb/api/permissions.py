7ed78f26efd6a96757c0805bec4062820720d4f6
Александр Джуманов <killer260598@mail.ru>
2023-02-05 15:53:26 +0300

    Added permission SuperUser/Admin/Moderator/Author

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -17,6 +17,17 @@ class IsSuperUserIsAdminIsModeratorIsAuthor(permissions.BasePermission):
     с ролью admin или moderator, а также автору объекта.
     """
 
+    def has_object_permission(self, request, view, obj):
+        return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+                and (request.user.is_superuser
+                     or request.user.is_staff
+                     or request.user.is_admin
+                     or request.user.is_moderator
+                     or request.user == obj.author)
+        )
+
 
 class IsAdmin(permissions.BasePermission):
     """Проверка, что админ или суперюзер"""
@@ -24,13 +35,13 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         user = request.user
         return (
-            user.is_authenticated and user.is_admin
-            or user.is_superuser
+                user.is_authenticated and user.is_admin
+                or user.is_superuser
         )
 
     def has_object_permission(self, request, view, obj):
         user = request.user
         return (
-            user.is_authenticated and user.is_admin
-            or user.is_superuser
+                user.is_authenticated and user.is_admin
+                or user.is_superuser
         )
