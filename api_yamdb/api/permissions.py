0f6fc5ce0e2ffc79cd554eb34e425488cfc6edcd
Юрий Голенков <yur.golenkoff@yandex.ru>
2023-02-05 16:49:46 +0300

    fix permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -18,15 +18,14 @@ class IsSuperUserIsAdminIsModeratorIsAuthor(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated
                 and (request.user.is_superuser
                      or request.user.is_staff
                      or request.user.is_admin
                      or request.user.is_moderator
                      or request.user == obj.author)
-        )
+                )
 
 
 class IsAdmin(permissions.BasePermission):
@@ -34,14 +33,12 @@ class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         user = request.user
-        return (
-                user.is_authenticated and user.is_admin
+        return (user.is_authenticated and user.is_admin
                 or user.is_superuser
-        )
+                )
 
     def has_object_permission(self, request, view, obj):
         user = request.user
-        return (
-                user.is_authenticated and user.is_admin
+        return (user.is_authenticated and user.is_admin
                 or user.is_superuser
-        )
+                )
